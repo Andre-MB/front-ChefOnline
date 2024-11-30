a6824f0ca66d7eeeb70c99a534ede6b028358a48
Andre-MB <andrezinhogame15@gmail.com>
2024-11-29 23:43:27 -0300

    update card category

--- pages/Category.jsx
+++ pages/Category.jsx
@@ -26,15 +26,15 @@ export default function Category() {
           />
         </div>
 
-        <div className=" text-white flex border-t border-white  mt-10">
-          <div className="flex p-2 w-full bg-slate-100 bg-opacity-20 rounded-lg m-3">
+        <div className="flex flex-wrap text-white border-t border-white mt-10">
+          <div className="flex p-2 w-max bg-slate-100 bg-opacity-20 w-1/2 rounded-lg m-2">
             <img
               className="imagemDoCard rounded-lg"
               src="../public/image.png"
               alt=""
             />
 
-            <div className="flex flex-col w-2/5 p-3 ">
+            <div className="flex flex-col w-3/4 p-3 ">
               <h1 className="text-xl text-green-500 font-bold">
                 Pudim simplão
               </h1>
@@ -45,8 +45,8 @@ export default function Category() {
               </p>
             </div>
 
-            <div className="flex flex-col gap-3 w-1/3">
-              <div className="flex flex-col">
+            <div className="flex flex-col gap-3 items-center">
+              <div className="flex flex-col w-max">
                 <div className="flex">
                   <img src="../public/black-star-silhouette-verde.png" alt="" />
                   <img src="../public/black-star-silhouette-cinza.png" alt="" />
@@ -62,7 +62,99 @@ export default function Category() {
                 </div>
               </div>
 
-              <div className="flex flex-col items-start">
+              <div className="flex flex-col items-start w-max">
+                <div className="flex flex-col items-center">
+                  <img src="../public/despertador2.png" width={30} alt="" />
+                  <h1>Tempo</h1>
+                </div>
+              </div>
+            </div>
+
+            {/*  */}
+          </div>
+
+          <div className="flex p-2 w-max bg-slate-100 bg-opacity-20 w-1/2 rounded-lg m-2">
+            <img
+              className="imagemDoCard rounded-lg"
+              src="../public/image.png"
+              alt=""
+            />
+
+            <div className="flex flex-col w-3/4 p-3 ">
+              <h1 className="text-xl text-green-500 font-bold">
+                Pudim simplão
+              </h1>
+              <p className="text-justify indent-8 pt-2">
+                Receita de Pudim de Leite Condensado da Mamãezinha, tradicional
+                e delicioso, feito com Leite MOÇA, Leite Líquido NINHO Forti+
+                Integral, ovos e calda de caramelo.
+              </p>
+            </div>
+
+            <div className="flex flex-col gap-3 items-center">
+              <div className="flex flex-col w-max">
+                <div className="flex">
+                  <img src="../public/black-star-silhouette-verde.png" alt="" />
+                  <img src="../public/black-star-silhouette-cinza.png" alt="" />
+                  <img src="../public/black-star-silhouette-cinza.png" alt="" />
+                </div>
+                <h1>Dificuldade</h1>
+              </div>
+
+              <div className="flex flex-col items-start w-max">
+                <div className="flex flex-col items-center">
+                  <img src="../public/prato.png" alt="" width={30} />
+                  <h1>Porções</h1>
+                </div>
+              </div>
+
+              <div className="flex flex-col items-start w-max">
+                <div className="flex flex-col items-center">
+                  <img src="../public/despertador2.png" width={30} alt="" />
+                  <h1>Tempo</h1>
+                </div>
+              </div>
+            </div>
+
+            {/*  */}
+          </div>
+
+          <div className="flex p-2 w-max bg-slate-100 bg-opacity-20 w-1/2 rounded-lg m-2">
+            <img
+              className="imagemDoCard rounded-lg"
+              src="../public/image.png"
+              alt=""
+            />
+
+            <div className="flex flex-col w-3/4 p-3 ">
+              <h1 className="text-xl text-green-500 font-bold">
+                Pudim simplão
+              </h1>
+              <p className="text-justify indent-8 pt-2">
+                Receita de Pudim de Leite Condensado da Mamãezinha, tradicional
+                e delicioso, feito com Leite MOÇA, Leite Líquido NINHO Forti+
+                Integral, ovos e calda de caramelo.
+              </p>
+            </div>
+
+            <div className="flex flex-col gap-3 items-center">
+              <div className="flex flex-col w-max">
+                <div className="flex">
+                  <img src="../public/black-star-silhouette-verde.png" alt="" />
+                  <img src="../public/black-star-silhouette-cinza.png" alt="" />
+                  <img src="../public/black-star-silhouette-cinza.png" alt="" />
+                </div>
+                <h1>Dificuldade</h1>
+              </div>
+
+              <div className="flex flex-col items-start w-max">
+                <div className="flex flex-col items-center">
+                  <img src="../public/prato.png" alt="" width={30} />
+                  <h1>Porções</h1>
+                </div>
+              </div>
+
+              <div className="flex flex-col items-start w-max">
                 <div className="flex flex-col items-center">
                   <img src="../public/despertador2.png" width={30} alt="" />
                   <h1>Tempo</h1>
