bb2e6dd38e2ade75ca6c7a71a331f555fb45cc89
Andre-MB <andrezinhogame15@gmail.com>
2024-12-06 23:06:40 -0300

    hover de card de Catgory

--- components/CardCatgory.jsx
+++ components/CardCatgory.jsx
@@ -56,7 +56,7 @@ export default function CardCatgory({ name, desc, dificult, pigs, time }) {
 
   return (
     <div>
-      <div className="flex p-2 bg-slate-100 bg-opacity-20 w-full rounded-lg ">
+      <div className="flex p-2 bg-slate-100 bg-opacity-20 w-full rounded-lg group hover:bg-slate-300 ">
         <img
           className="imagemDoCard rounded-lg"
           src="../public/image.png"
@@ -64,23 +64,27 @@ export default function CardCatgory({ name, desc, dificult, pigs, time }) {
         />
 
         <div className="flex flex-col w-3/4 p-3 ">
-          <h1 className="text-xl text-green-500 font-bold">{name}</h1>
-          <p className="text-justify indent-8 pt-2">{desc}</p>
+          <h1 className="text-xl text-green-500 font-bold group-hover:text-green-800">
+            {name}
+          </h1>
+          <p className="text-justify indent-8 pt-2 group-hover:text-gray-700">
+            {desc}
+          </p>
         </div>
 
         <div className="flex flex-col gap-3 items-center">
           <div className="flex flex-col w-max">
             <div className="flex">{renderContent()}</div>
-            <h1>Dificuldade</h1>
+            <h1 className="group-hover:text-green-600">Dificuldade</h1>
           </div>
 
           <div className="flex flex-col items-start w-max">
             <div className="flex flex-col items-center">
               <div className="flex gap-2">
                 <img src="../public/prato.png" alt="" width={30} />
-                <h1>{pigs}</h1>
+                <h1 className="group-hover:text-gray-700">{pigs}</h1>
               </div>
-              <h1>Porções</h1>
+              <h1 className="group-hover:text-green-600">Porções</h1>
             </div>
           </div>
 
@@ -88,9 +92,9 @@ export default function CardCatgory({ name, desc, dificult, pigs, time }) {
             <div className="flex flex-col items-center">
               <div className="flex gap-2">
                 <img src="../public/despertador2.png" width={30} alt="" />
-                <h1>{time}</h1>
+                <h1 className="group-hover:text-gray-700">{time}</h1>
               </div>
-              <h1>Tempo</h1>
+              <h1 className="group-hover:text-green-600">Tempo</h1>
             </div>
           </div>
         </div>
