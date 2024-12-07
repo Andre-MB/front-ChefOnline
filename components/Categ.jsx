1c3a31925ed7684b6a5b23f5659ff4c86cda5a2e
Andre-MB <andrezinhogame15@gmail.com>
2024-12-07 00:02:31 -0300

    add Tailwind css Animated

--- components/Categ.jsx
+++ components/Categ.jsx
@@ -9,12 +9,15 @@ export default function Categ({ imagem, name }) {
   return (
     <>
       <Link to={`/category/${name}`}>
-        <div className="group hover:scale-110" onClick={navegacao}>
+        <div
+          className="group hover:scale-110 ease-in-out  relative"
+          onClick={navegacao}
+        >
           <img className="imageCard rounded-2xl" src={imagem} alt="" />
-          <h1 className="titulo text-white text-2xl relative z-10 hidden group-hover:flex transition">
+          <h1 className="titulo text-white text-2xl relative z-10 hidden group-hover:flex group-hover:animate-fade-up animate-duration-[500ms] animate-ease-in-out">
             {name}
           </h1>
-          <div className="sombra relative z-1 hidden group-hover:flex transition"></div>
+          <div className="sombra relative z-1 hidden group-hover:flex duration-1000 transform ease-in-out translate-y-100 opacity-0 group-hover:translate-y-0 group-hover:opacity-100"></div>
         </div>
       </Link>
     </>
